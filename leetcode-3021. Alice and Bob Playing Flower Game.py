40a091562beb3afa1382e7a0631b75f0fa8f6427
Nishant Gola <nishant.gola.work@gmail.com>
2026-01-10 18:16:03 +0530

    modified leetcode-3021. Alice and Bob Playing Flower Game

--- leetcode-3021. Alice and Bob Playing Flower Game.py
+++ leetcode-3021. Alice and Bob Playing Flower Game.py
@@ -1,5 +1,5 @@
 class Solution:
-    def flower(self,n:int,m:int):
+    def flower(self,n:int,m:int)->int:
         # number of odd values from 1 to n
         odd_n=(n+1)//2
         # number of odd values from 1 to m
@@ -8,7 +8,8 @@ class Solution:
         even_n=m//2
         # number of even values from 1 to m
         even_m=n//2
-
+        
+        #total number of ways the alice wins
         return int(odd_n*even_m+even_n*odd_m)
 
     
